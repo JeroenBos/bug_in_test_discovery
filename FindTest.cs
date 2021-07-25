bc2e70d4f46632094de1526e0ee369f713c6b016
Jeroen Bos <jeroen@deepinsight.io>
2021-07-25 13:06:51 +0200

    Add project files.

---
+++ FindTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xunit_bug_testfilter
+{
+	public class FindTest
+	{
+		[Fact]
+		public void FindNameOfTest()
+		{
+			var typesDerivingFromMyTestComponent = typeof(FindTest).Assembly
+											                       .GetTypes()
+											                       .Where(typeof(MyTestComponent).IsAssignableFrom)
+											                       .Where(type => type != typeof(MyTestComponent))
+											                       .ToList();
+
+			Assert.Equal(1, typesDerivingFromMyTestComponent.Count);
+			Assert.Equal("xunit_bug_testfilter.Test__", typesDerivingFromMyTestComponent[0].FullName);
+		}
+	}
+}
---
+++ MyTestComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Rendering;
+using Xunit;
+
+public abstract class MyTestComponent
+{
+	protected virtual void BuildRenderTree(RenderTreeBuilder builder) { }
+	[Fact]
+	public void RazorTests() { }
+}
